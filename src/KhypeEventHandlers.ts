564577e1f344851cd23e68a5a0407e08e2f236fa
Nikhil Bhintade <29760727+nikbhintade@users.noreply.github.com>
2025-08-13 12:29:40 +0000

    updated handler & coverage setup

--- src/KhypeEventHandlers.ts
+++ src/KhypeEventHandlers.ts
@@ -1,4 +1,13 @@
 import { KHYPE, User } from "generated";
+import { storageID } from "./constants";
+
+function safeStringify(obj: any) {
+    return JSON.stringify(
+        obj,
+        (_k, v) => typeof v === "bigint" ? `${v.toString()}n` : v,
+    );
+}
+
 
 KHYPE.Transfer.handler(async ({ event, context }) => {
     const amount = event.params.value;
@@ -10,13 +19,77 @@ KHYPE.Transfer.handler(async ({ event, context }) => {
     const sender = await context.User.get(from);
     const receiver = await context.User.get(to);
 
+    // increase total supply
+    if (from === zeroAddress) {
+        // get last block
+        const storage = await context.Storage.get(storageID);
+
+        // get totalSupplySnapshot for last block
+        const lastSnapshot = await context.TotalSupplySnapshot.get(
+            storage?.lastBlockNumber.toString() || "0",
+        );
+
+        // if no snapshot exists, create one
+        if (!lastSnapshot) {
+            console.log(
+                `Creating new TotalSupplySnapshot for block ${event.block.number}`,
+            );
+            // create a new snapshot
+            context.TotalSupplySnapshot.set({
+                id: event.block.number.toString(),
+                totalSupply: BigInt(amount),
+            });
+        } else {
+            // update the existing snapshot
+            context.TotalSupplySnapshot.set({
+                ...lastSnapshot,
+                totalSupply: lastSnapshot.totalSupply + amount,
+            });
+        }
+
+        // update storage with the new block number
+        context.Storage.set({
+            id: storageID,
+            lastBlockNumber: event.block.number,
+        });
+    }
+
+    // decrease total supply
+    if (to === zeroAddress) {
+        // get last block
+        const storage = await context.Storage.get(storageID);
+
+        // get totalSupplySnapshot for last block
+        const lastSnapshot = await context.TotalSupplySnapshot.get(
+            storage?.lastBlockNumber.toString() || "0",
+        );
+
+        // if no snapshot exists, create one
+        if (!lastSnapshot) {
+            context.TotalSupplySnapshot.set({
+                id: event.block.number.toString(),
+                totalSupply: BigInt(amount),
+            });
+        } else {
+            // update the existing snapshot
+            context.TotalSupplySnapshot.set({
+                ...lastSnapshot,
+                totalSupply: lastSnapshot.totalSupply - amount,
+            });
+        }
+
+        // update storage with the new block number
+        context.Storage.set({
+            id: storageID,
+            lastBlockNumber: event.block.number,
+        });
+    }
+
     // Update sender balance only if not zero address
+    
     if (from !== zeroAddress) {
         if (sender) {
-            context.User.set({
-                ...sender,
-                balance: sender.balance - amount,
-            });
+            
         } else {
             console.warn(`Unexpected non-zero sender: ${from}`);
             context.User.set({
---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const storageID =
+    "0x2790f8d27e5c86a79444e83d3da9039fe841e87a6b995a8537a5e72b56ec1b35";
